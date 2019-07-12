a5b6139b80a6dd38422cf05f8729813d496d11b8
asharikov <xakepok@xakepok.com>
2019-07-12 18:38:06 +0300

    Fluid

--- components/com_rw/views/stations/tmpl/default.php
+++ components/com_rw/views/stations/tmpl/default.php
@@ -12,62 +12,64 @@ HTMLHelper::_('stylesheet', '//cdn.datatables.net/1.10.19/css/jquery.dataTables.
 HTMLHelper::_('stylesheet', '//cdn.datatables.net/fixedcolumns/3.2.6/css/fixedColumns.dataTables.min.css', array('version' => 'auto', 'relative' => true));
 ?>
 <h1><?php echo JText::sprintf('COM_RW_TITLE_DIRECTION', $this->direction);?></h1>
-<table class="table table-sm" style="width: 100%;">
-    <thead>
-        <tr>
-            <th>
-                <?php echo JText::sprintf('COM_RW_HEAD_STATION_TITLE');?>
-            </th>
-            <th>
-                <?php echo JText::sprintf('COM_RW_HEAD_STATION_ZONE_ID');?>
-            </th>
-            <?php if (!JFactory::getApplication()->client->mobile): ?>
-                <th>
-                    <?php echo JText::sprintf('COM_RW_HEAD_STATION_ZONE_DISTANCE');?>
-                </th>
-            <?php endif;?>
-            <?php if ($this->isClub !== false): ?>
-                <th>
-                    <?php echo JText::sprintf('COM_RW_MODAL_TITLE_DESCS');?>
-                </th>
-            <?php endif;?>
-            <th>
-                <?php echo JText::sprintf('COM_RW_HEAD_STATION_ZONE_TURNSTILES');?>
-            </th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($this->items as $item) :?>
+<div class="container-fluid">
+    <table class="table table-sm" style="width: 100%;">
+        <thead>
             <tr>
-                <td><?php echo $item['station'];?></td>
-                <td><?php echo $item['zoneID'];?></td>
+                <th>
+                    <?php echo JText::sprintf('COM_RW_HEAD_STATION_TITLE');?>
+                </th>
+                <th>
+                    <?php echo JText::sprintf('COM_RW_HEAD_STATION_ZONE_ID');?>
+                </th>
                 <?php if (!JFactory::getApplication()->client->mobile): ?>
-                    <td><?php echo $item['distance'];?></td>
+                    <th>
+                        <?php echo JText::sprintf('COM_RW_HEAD_STATION_ZONE_DISTANCE');?>
+                    </th>
                 <?php endif;?>
                 <?php if ($this->isClub !== false): ?>
-                    <td>
-                        <?php if (isset($item['descs']) && $item['descs'][0]['tppd'] !== true && $item['descs'][0]['everyday'] !== true && $item['descs'][0]['no_desc'] !== true): ?>
-                            <a href="#" data-toggle="modal" data-target="#modal_desc_<?php echo $item['id'];?>"><?php echo JText::sprintf('COM_RW_MODAL_TITLE_DESCS');?></a>
-                        <?php endif;?>
-                        <?php if (isset($item['descs']) && $item['descs'][0]['tppd'] === true): ?>
-                            <?php echo JText::sprintf('COM_RW_DESC_TPPD');?>
-                        <?php endif;?>
-                        <?php if (isset($item['descs']) && $item['descs'][0]['time_mask'] === null): ?>
-                            <?php echo JText::sprintf('COM_RW_DESC_UNKNOWN');?>
-                        <?php endif;?>
-                        <?php if (isset($item['descs']) && $item['descs'][0]['everyday'] === true): ?>
-                            <?php echo JText::sprintf('COM_RW_DESC_TIME_MASK_EVERYDAY');?>
-                        <?php endif;?>
-                        <?php if (isset($item['descs']) && $item['descs'][0]['no_desc'] === true): ?>
-                            <?php echo JText::sprintf('COM_RW_DESC_NO_DESC');?>
-                        <?php endif;?>
-                    </td>
+                    <th>
+                        <?php echo JText::sprintf('COM_RW_MODAL_TITLE_DESCS');?>
+                    </th>
                 <?php endif;?>
-                <td><?php echo $item['turnstiles'];?></td>
+                <th>
+                    <?php echo JText::sprintf('COM_RW_HEAD_STATION_ZONE_TURNSTILES');?>
+                </th>
             </tr>
-        <?php endforeach;?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            <?php foreach ($this->items as $item) :?>
+                <tr>
+                    <td><?php echo $item['station'];?></td>
+                    <td><?php echo $item['zoneID'];?></td>
+                    <?php if (!JFactory::getApplication()->client->mobile): ?>
+                        <td><?php echo $item['distance'];?></td>
+                    <?php endif;?>
+                    <?php if ($this->isClub !== false): ?>
+                        <td>
+                            <?php if (isset($item['descs']) && $item['descs'][0]['tppd'] !== true && $item['descs'][0]['everyday'] !== true && $item['descs'][0]['no_desc'] !== true): ?>
+                                <a href="#" data-toggle="modal" data-target="#modal_desc_<?php echo $item['id'];?>"><?php echo JText::sprintf('COM_RW_MODAL_TITLE_DESCS');?></a>
+                            <?php endif;?>
+                            <?php if (isset($item['descs']) && $item['descs'][0]['tppd'] === true): ?>
+                                <?php echo JText::sprintf('COM_RW_DESC_TPPD');?>
+                            <?php endif;?>
+                            <?php if (isset($item['descs']) && $item['descs'][0]['time_mask'] === null): ?>
+                                <?php echo JText::sprintf('COM_RW_DESC_UNKNOWN');?>
+                            <?php endif;?>
+                            <?php if (isset($item['descs']) && $item['descs'][0]['everyday'] === true): ?>
+                                <?php echo JText::sprintf('COM_RW_DESC_TIME_MASK_EVERYDAY');?>
+                            <?php endif;?>
+                            <?php if (isset($item['descs']) && $item['descs'][0]['no_desc'] === true): ?>
+                                <?php echo JText::sprintf('COM_RW_DESC_NO_DESC');?>
+                            <?php endif;?>
+                        </td>
+                    <?php endif;?>
+                    <td><?php echo $item['turnstiles'];?></td>
+                </tr>
+            <?php endforeach;?>
+        </tbody>
+    </table>
+</div>
 <div>
     <?php echo $this->loadTemplate('descs');?>
 </div>
