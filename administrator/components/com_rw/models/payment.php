a9e7a73abc1572ce833bb1d5a0aadf116e4ef9a8
xakepok@xakepok.com <44duP_yx579>
2019-07-13 19:02:34 +0300

    Payment model

---
+++ administrator/components/com_rw/models/payment.php
@@ -0,0 +1,77 @@
+<?php
+defined('_JEXEC') or die;
+use Joomla\CMS\MVC\Model\AdminModel;
+
+class RwModelPayment extends AdminModel {
+    public function getTable($name = 'Payments', $prefix = 'TableRw', $options = array())
+    {
+        return JTable::getInstance($name, $prefix, $options);
+    }
+
+    public function delete(&$pks)
+    {
+        return parent::delete($pks);
+    }
+
+    public function getItem($pk = null)
+    {
+        return parent::getItem($pk);
+    }
+
+    public function getForm($data = array(), $loadData = true)
+    {
+        $form = $this->loadForm(
+            $this->option.'.payment', 'payment', array('control' => 'jform', 'load_data' => $loadData)
+        );
+        if (empty($form))
+        {
+            return false;
+        }
+        return $form;
+    }
+
+    protected function loadFormData()
+    {
+        $data = JFactory::getApplication()->getUserState($this->option.'.edit.payment.data', array());
+        if (empty($data))
+        {
+            $data = $this->getItem();
+        }
+
+        return $data;
+    }
+
+    public function save($data)
+    {
+        return parent::save($data);
+    }
+
+    protected function prepareTable($table)
+    {
+        $nulls = array('sender'); //Поля, которые NULL
+        foreach ($nulls as $field)
+        {
+            if (!strlen($table->$field)) $table->$field = NULL;
+        }
+        parent::prepareTable($table);
+    }
+
+    protected function canEditState($record)
+    {
+        $user = JFactory::getUser();
+
+        if (!empty($record->id))
+        {
+            return $user->authorise('core.edit.state', $this->option . '.payment.' . (int) $record->id);
+        }
+        else
+        {
+            return parent::canEditState($record);
+        }
+    }
+
+    public function getScript()
+    {
+        return 'administrator/components/' . $this->option . '/models/forms/payment.js';
+    }
+}
