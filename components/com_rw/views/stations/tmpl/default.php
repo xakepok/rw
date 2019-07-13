55298941059f57c75e1bad1727b6c51d80490918
xakepok@xakepok.com <44duP_yx579>
2019-07-13 17:42:23 +0300

    Advert for no-club in desc

--- components/com_rw/views/stations/tmpl/default.php
+++ components/com_rw/views/stations/tmpl/default.php
@@ -1,4 +1,5 @@
 <?php
+
 use Joomla\CMS\HTML\HTMLHelper;
 
 defined('_JEXEC') or die;
@@ -11,7 +12,7 @@ HTMLHelper::_('script', 'com_rw/tdata.js', array('version' => 'auto', 'relative'
 HTMLHelper::_('stylesheet', '//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css', array('version' => 'auto', 'relative' => true));
 HTMLHelper::_('stylesheet', '//cdn.datatables.net/fixedcolumns/3.2.6/css/fixedColumns.dataTables.min.css', array('version' => 'auto', 'relative' => true));
 ?>
-<h1><?php echo JText::sprintf('COM_RW_TITLE_DIRECTION', $this->direction);?></h1>
+<h1><?php echo JText::sprintf('COM_RW_TITLE_DIRECTION', $this->direction); ?></h1>
 <div class="container">
     <div class="row">
         <div class="col">
@@ -19,61 +20,66 @@ HTMLHelper::_('stylesheet', '//cdn.datatables.net/fixedcolumns/3.2.6/css/fixedCo
                 <thead>
                 <tr>
                     <th>
-                        <?php echo JText::sprintf('COM_RW_HEAD_STATION_TITLE');?>
+                        <?php echo JText::sprintf('COM_RW_HEAD_STATION_TITLE'); ?>
                     </th>
                     <th>
-                        <?php echo JText::sprintf('COM_RW_HEAD_STATION_ZONE_ID');?>
+                        <?php echo JText::sprintf('COM_RW_HEAD_STATION_ZONE_ID'); ?>
                     </th>
                     <?php if (!JFactory::getApplication()->client->mobile): ?>
                         <th>
-                            <?php echo JText::sprintf('COM_RW_HEAD_STATION_ZONE_DISTANCE');?>
+                            <?php echo JText::sprintf('COM_RW_HEAD_STATION_ZONE_DISTANCE'); ?>
                         </th>
-                    <?php endif;?>
-                    <?php if ($this->isClub !== false): ?>
-                        <th>
-                            <?php echo JText::sprintf('COM_RW_MODAL_TITLE_DESCS');?>
-                        </th>
-                    <?php endif;?>
+                    <?php endif; ?>
                     <th>
-                        <?php echo JText::sprintf('COM_RW_HEAD_STATION_ZONE_TURNSTILES');?>
+                        <?php echo JText::sprintf('COM_RW_MODAL_TITLE_DESCS'); ?>
+                    </th>
+                    <th>
+                        <?php echo JText::sprintf('COM_RW_HEAD_STATION_ZONE_TURNSTILES'); ?>
                     </th>
                 </tr>
                 </thead>
                 <tbody>
-                <?php foreach ($this->items as $item) :?>
+                <?php foreach ($this->items as $item) : ?>
                     <tr>
-                        <td><?php echo $item['station'];?></td>
-                        <td><?php echo $item['zoneID'];?></td>
+                        <td><?php echo $item['station']; ?></td>
+                        <td><?php echo $item['zoneID']; ?></td>
                         <?php if (!JFactory::getApplication()->client->mobile): ?>
-                            <td><?php echo $item['distance'];?></td>
-                        <?php endif;?>
+                            <td><?php echo $item['distance']; ?></td>
+                        <?php endif; ?>
                         <?php if ($this->isClub !== false): ?>
                             <td>
                                 <?php if (isset($item['descs']) && $item['descs'][0]['tppd'] !== true && $item['descs'][0]['everyday'] !== true && $item['descs'][0]['no_desc'] !== true): ?>
-                                    <a href="#" data-toggle="modal" data-target="#modal_desc_<?php echo $item['id'];?>"><?php echo JText::sprintf('COM_RW_MODAL_TITLE_DESCS');?></a>
-                                <?php endif;?>
+                                    <a href="#" data-toggle="modal"
+                                       data-target="#modal_desc_<?php echo $item['id']; ?>"><?php echo JText::sprintf('COM_RW_MODAL_TITLE_DESCS'); ?></a>
+                                <?php endif; ?>
                                 <?php if (isset($item['descs']) && $item['descs'][0]['tppd'] === true): ?>
-                                    <?php echo JText::sprintf('COM_RW_DESC_TPPD');?>
-                                <?php endif;?>
+                                    <?php echo JText::sprintf('COM_RW_DESC_TPPD'); ?>
+                                <?php endif; ?>
                                 <?php if (isset($item['descs']) && $item['descs'][0]['time_mask'] === null): ?>
-                                    <?php echo JText::sprintf('COM_RW_DESC_UNKNOWN');?>
-                                <?php endif;?>
+                                    <?php echo JText::sprintf('COM_RW_DESC_UNKNOWN'); ?>
+                                <?php endif; ?>
                                 <?php if (isset($item['descs']) && $item['descs'][0]['everyday'] === true): ?>
-                                    <?php echo JText::sprintf('COM_RW_DESC_TIME_MASK_EVERYDAY');?>
-                                <?php endif;?>
+                                    <?php echo JText::sprintf('COM_RW_DESC_TIME_MASK_EVERYDAY'); ?>
+                                <?php endif; ?>
                                 <?php if (isset($item['descs']) && $item['descs'][0]['no_desc'] === true): ?>
-                                    <?php echo JText::sprintf('COM_RW_DESC_NO_DESC');?>
-                                <?php endif;?>
+                                    <?php echo JText::sprintf('COM_RW_DESC_NO_DESC'); ?>
+                                <?php endif; ?>
                             </td>
-                        <?php endif;?>
-                        <td><?php echo $item['turnstiles'];?></td>
+                        <?php endif; ?>
+                        <?php if ($this->isClub === false): ?>
+                            <td>
+                                <a href="#" data-toggle="modal"
+                                   data-target="#modal_desc_ad"><?php echo JText::sprintf('COM_RW_MODAL_TITLE_DESCS'); ?></a>
+                            </td>
+                        <?php endif; ?>
+                        <td><?php echo $item['turnstiles']; ?></td>
                     </tr>
-                <?php endforeach;?>
+                <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
     </div>
     <div>
-        <?php echo $this->loadTemplate('descs');?>
+        <?php echo $this->loadTemplate('descs'); ?>
     </div>
 </div>
--- components/com_rw/views/stations/tmpl/default_descs.php
+++ components/com_rw/views/stations/tmpl/default_descs.php
@@ -32,3 +32,28 @@ foreach ($this->items as $item):
     </div>
 </div>
 <?php endforeach;?>
+
+
+<?php if ($this->isClub === false): ?>
+    <div class="modal fade" id="modal_desc_ad" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalCenterTitle"><?php echo JText::sprintf('COM_RW_MODAL_TITLE_DESCS');?></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">X</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <img src="/images/super-zero.png" alt="Супер-ноль" class="w-100" />
+                    Информация о времени работы касс находится на сервере, обслуживание котрого стоит денег. Авторизуйтесь на сайте, сделайте донат и вы получите абонемент "Супер ноль", владельцам которого доступна информация о кассах и ТППД. Более того, сайт развивается и постепенно владельцам абонемента будут доступны интерактивная карта и другие функции.<br>
+                    Стоимость абонемента всего 36р в месяц, как стоимость проезда по Москве на электричках. Все деньги идут на оплату сервера.<br>
+                    Приобретая абонемент, вы помогаете и себе, и любимому собакингу.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><?php echo JText::sprintf('COM_RW_ACTION_CLOSE');?></button>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endif;?>
